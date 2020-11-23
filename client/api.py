d21442af0318ca7dd5e578cd775cbf55010bda97
Mascarenhas12 <manuel.d.mascarenhas@tecnico.ulisboa.pt>
2020-11-23 11:55:36 +0000

    Register and Login Done

--- client/api.py
+++ client/api.py
@@ -1,11 +1,36 @@
 import requests
 
+log_server_ip = "http://192.168.57.10"
+session_token = ""
+pubkey = "something"  # Go get from file
+
+
+def is_response_status_ok(response) -> bool:
+    return 200 < response.status_code < 300
+
 
 def register(username: str, password: str) -> bool:
     """
+    - Get pub key
     - Create HTTP request with username and password
     """
-    pass
+    register_data = {
+        "username": username,
+        "password": password,
+        "pubkey": pubkey
+    }
+
+    response = requests.post("{}/api/user/register/".format(log_server_ip),
+                             data=register_data
+                             )
+
+    if is_response_status_ok(response):
+        global session_token
+        session_token = response.json()["token"]
+        return True
+
+    print("Http response not ok: {}".format(response.json()))
+    return False
 
 
 def login(username: str, password: str) -> bool:
@@ -13,10 +38,25 @@ def login(username: str, password: str) -> bool:
     - Create HTTP request with username and password
     - Save token (session) login
     """
-    pass
+    login_data = {
+        "username": username,
+        "password": password
+    }
+
+    response = requests.post("{}/api/user/register/".format(log_server_ip),
+                             data=login_data
+                             )
+
+    if is_response_status_ok(response):
+        global session_token
+        session_token = response.json()["token"]
+        return True
+
+    print("Http response not ok: {}".format(response.json()))
+    return False
 
 
-def create_file(filepath: str, keys: list, contributors=[]) -> bool:
+def create_file(filepath: str, keys: list, contributors=None) -> bool:
     """
     - Get file from file path
     - Encrypt it with PGP
@@ -25,6 +65,8 @@ def create_file(filepath: str, keys: list, contributors=[]) -> bool:
     - Create HTTP request with encrypted file, list of user keys encrypted, list of contributors
     - Save file_id
     """
+    if contributors is None:
+        contributors = []
     pass
 
 
