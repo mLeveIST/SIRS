3f3f745ac2b7e822194dfa153089b2fae5a8dbd5
Miguel Leve <miguel.levezinho@tecnico.ulisboa.pt>
2020-11-27 14:08:36 +0000

    Implemented PGP protocol
    - Uses AES CBC Mode for file encryption
    - Used RSA keys for AES key encryption
    - Uses RSA keys for authentication using a signature

--- client/main.py
+++ client/main.py
@@ -1,12 +1,16 @@
 from sys import argv
-from pgp import encrypt_PGP_AES_GCM
+from pgp import encrypt_PGP_AES_GCM, encrypt_PGP_AES_CBC, decrypt_PGP_AES_CBC
 
 def main(args: list):
 	data = b'Faz o PHP Manel!'
 	version = b'1'
 
-	to_send = encrypt_PGP_AES_GCM(data, version)
-	print(to_send)
+	to_send = encrypt_PGP_AES_CBC(data, version)
+	print(f"\nClient PGP Encryption:\n- edata: {to_send[0]}\n- ekey: {to_send[1]}")
+	print(f"- version: {to_send[2]}\n- signature: {to_send[3]}")
+
+	received_data = decrypt_PGP_AES_CBC(to_send[0], to_send[1])
+	print(f"\nClient PGP Decryption:\n- {received_data}\n")
 
 
 if __name__ == "__main__":
--- client/pgp.py
+++ client/pgp.py
@@ -4,15 +4,119 @@ from cryptography.hazmat.primitives.ciphers import Cipher, modes
 from cryptography.hazmat.primitives.ciphers.algorithms import AES
 
 from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.padding import PKCS7
 from cryptography.hazmat.primitives.asymmetric import padding, rsa, utils
 import hashlib
 
 from sys import stderr
 import os
 
+MAX_DATA_SIZE = 4294967296 # 4GB (TEMP)
+RAND_KEY_SIZE = 256 # bits
+BLOCK_SIZE = 128 # bits
+
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+public_key = private_key.public_key()
+
+
+
+def encrypt_PGP_AES_CBC(data: bytes, version: bytes):
+	random_key = os.urandom(RAND_KEY_SIZE // 8)
+	iv = os.urandom(BLOCK_SIZE // 8)
+
+	if len(data) > MAX_DATA_SIZE: # TEMP
+		error(f"Data is too big (exceeds 4GB)!")
+		return
+
+	# Padding
+	padder = PKCS7(BLOCK_SIZE).padder()
+	padded_data = padder.update(data) + padder.finalize()
+
+	# Encrypt file data
+	cipher = Cipher(AES(random_key), modes.CBC(iv)).encryptor()
+	edata = cipher.update(padded_data) + cipher.finalize()
+
+	# Encrypt random key and IV
+	key = random_key + iv # <------ Is doing this secure ?
+	ekey = public_key.encrypt(
+		key,
+		padding.OAEP(
+			mgf=padding.MGF1(algorithm=hashes.SHA256()),
+			algorithm=hashes.SHA256(),
+			label=None
+		)
+	)
+
+	# Hash edata, ekey, and file version
+	hashfunc = hashes.Hash(hashes.SHA256())
+	hashfunc.update(edata)
+	hashfunc.update(ekey)
+	hashfunc.update(version)
+	digest = hashfunc.finalize()
+
+	# Sign digest
+	signature = private_key.sign(
+		digest,
+		padding.PSS(
+			mgf=padding.MGF1(hashes.SHA256()),
+			salt_length=padding.PSS.MAX_LENGTH
+		),
+		utils.Prehashed(hashes.SHA256())
+	)
+
+	return (edata, ekey, version, signature)
+
+
+def decrypt_PGP_AES_CBC(edata: bytes, ekey: bytes):
+
+	# Decrypt random key and IV
+	key = private_key.decrypt(
+		ekey,
+		padding.OAEP(
+			mgf=padding.MGF1(algorithm=hashes.SHA256()),
+			algorithm=hashes.SHA256(),
+			label=None
+		)
+	)
+	random_key = key[:BLOCK_SIZE // -8]
+	iv = key[BLOCK_SIZE // -8:]
+
+	# Decrypt file data
+	cipher = Cipher(AES(random_key), modes.CBC(iv)).decryptor()
+	padded_data = cipher.update(edata) + cipher.finalize()
+
+	# Unpadding
+	unpadder = PKCS7(BLOCK_SIZE).unpadder()
+	data = unpadder.update(padded_data) + unpadder.finalize()
+
+	return data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Also provides authenticaton by itself, but we will still need to hash to authenticate key and version together
-# Redundancy is good ?
 def encrypt_PGP_AES_GCM(data: bytes, version: bytes) -> tuple:
 
 	random_key = AESGCM.generate_key(bit_length=256) # also uses urandom()
@@ -34,10 +138,6 @@ def encrypt_PGP_AES_GCM(data: bytes, version: bytes) -> tuple:
 	# 	)
 	#
 	# public_key = private_key.public_key() # Also read from file ?
-
-	# TEMP
-	private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-	public_key = private_key.public_key()
 
 	# Encrypt random key
 	ekey = public_key.encrypt(
@@ -68,17 +168,3 @@ def encrypt_PGP_AES_GCM(data: bytes, version: bytes) -> tuple:
 
 	return (edata, ekey, version, signature)
 
-
-
-def decrypt_PGP_AES_GCM(edata: bytes, ekey: bytes):
-	pass # TODO
-
-
-# Does not provide authentication
-# Needs padding, so is suceptible to padding oracle attacks
-def encrypt_PGP_AES_CBC(data: bytes):
-	random_key = os.urandom(32)
-	iv = os.urandom(16)
-	cipher = Cipher(AES(random_key), modes.CBC(iv))
-
-	# ...
