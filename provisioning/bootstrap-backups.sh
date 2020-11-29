c66a49f4148f3666015143f4dc6374c545484e42
Mascarenhas12 <manuel.d.mascarenhas@tecnico.ulisboa.pt>
2020-11-29 23:48:03 +0000

    Refactor provision and nfs.yaml

--- provisioning/bootstrap-backups.sh
+++ provisioning/bootstrap-backups.sh
@@ -12,15 +12,14 @@ sudo apt-get -y python-dev
 sudo apt-get -y python-pip 
 sudo apt-get -y python3-pip
 
-# Add graph builder tool for Terraform
-sudo apt-get -y install graphviz
-
-# install ansible (http://docs.ansible.com/intro_installation.html)
-apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get -y install ansible
-
 sudo apt-get update && sudo apt-get upgrade -y
+
+# install key
+cat /vagrant/control/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+cp /vagrant/control/root* /home/vagrant/
+sudo mkdir -p /var/repo
+cp -R /vagrant/backupserver /var/repo
+sudo chown -R www-data:www-data /var/repo/backupserver
 
 # configure hosts file for the internal network defined by Vagrantfile
 cat >> /etc/hosts <<EOL
@@ -31,8 +30,4 @@ cat >> /etc/hosts <<EOL
 192.168.57.13   file
 192.168.57.11   bs1
 192.168.57.12   bs2
-192.168.59.11   client1
-192.168.59.12   client2
-192.168.59.13   client3
-192.168.59.14   client4
 EOL
